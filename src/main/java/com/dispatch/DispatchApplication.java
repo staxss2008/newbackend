97b079a5ea3cc1eb112770b29a9c084c4970b81d
staxss <tal_cfd@163.com>
2026-01-23 10:48:13 +0800

    Fix database configuration to use Railway MYSQL_PUBLIC_URL

--- src/main/java/com/dispatch/DispatchApplication.java
+++ src/main/java/com/dispatch/DispatchApplication.java
@@ -3,11 +3,12 @@ package com.dispatch;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
- * 派车管理系统启动类
+ * 调度管理系统启动类
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan("com.dispatch.system.mapper")
 public class DispatchApplication {
     public static void main(String[] args) {
